82e0054bd66e602d84065d3e350a04fcb7c220b7
kristinatibeikina <kristina.tibeykina04@mail.ru>
2024-03-19 15:43:54 +0700

    Добавление роли студент

---
+++ cypress/e2e/roleStudent/positive Role.cy.js
@@ -0,0 +1,27 @@
+describe('Authorization Tests',()=>{
+  it('positive authorization test', ()=>{
+    cy.fixture('example').then(data=>{
+      cy.log('Переход на сайт')
+      cy.visit(data.main_url)
+
+      cy.get('[href="/login"] > .button').should('be.visible').click()
+
+      cy.log('Ввод логина')
+      cy.get('.form-input--text')
+        .type(data.existent_login)
+
+      cy.log('Ввод пароля')
+      cy.get('.form-input--password')
+        .type(data.existent_password)
+
+      cy.log('Отправка данных на проверку')
+      cy.get(':nth-child(3) > .button').should('be.visible').click()
+
+      cy.log('Переход на выбор роли')
+      cy.get('.page-nav__role-block > .button').should('be.visible').click()
+
+      cy.log('Выбираем роль студент')
+      cy.get('.select-role-form > :nth-child(3)').should('be.visible').click()
+    })
+  })
+})
