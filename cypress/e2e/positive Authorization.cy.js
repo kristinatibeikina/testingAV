8ffd926fe86acdab791e55c8822b824648bce861
kristinatibeikina <kristina.tibeykina04@mail.ru>
2024-03-19 13:10:28 +0700

    Позитивная авторизация

---
+++ cypress/e2e/positive Authorization.cy.js
@@ -0,0 +1,20 @@
+describe('Registration Tests',()=>{
+  it('positive registration test', ()=>{
+    cy.fixture('example').then(data=>{
+      cy.log('Переход на сайт')
+      cy.visit(data.main_url)
+
+      cy.get('[href="/login"] > .button').should('be.visible').click()
+
+      cy.log('Ввод логина')
+      cy.get('.form-input--text')
+        .type(data.existent_login)
+
+      cy.log('Ввод пароля')
+      cy.get('.form-input--password')
+        .type(data.existent_password)
+
+      cy.get('.form__buttons > :nth-child(3)').should('be.visible').click()
+    })
+  })
+})
