ecef8501e173c32da57811f1ca1d3d52a5ef2c66
kristinatibeikina <kristina.tibeykina04@mail.ru>
2024-03-19 19:43:35 +0700

    Негативное подтверждение роли студента

---
+++ cypress/e2e/roleStudent/negative Role.cy.js
@@ -0,0 +1,23 @@
+describe('Role Tests',()=>{
+  it('negative role test', ()=>{
+    cy.fixture('example').then(data=>{
+      cy.log('Переход на сайт')
+      cy.visit(data.main_url)
+
+      cy.get('[href="/login"] > .button').should('be.visible').click()
+
+      cy.log('Ввод логина')
+      cy.get('.form-input--text')
+        .type(data.none_authorization_login)
+
+      cy.log('Ввод пароля')
+      cy.get('.form-input--password')
+        .type(data.none_authorization_password)
+
+      cy.log('Отправка данных на проверку')
+      cy.get(':nth-child(3) > .button').should('be.visible').click()
+
+  
+    })
+  })
+})
